28adea8b5509e651ac6596e38cf6c5148e551f9a
mrkyel <kyel100@naver.com>
2020-02-27 17:18:15 +0900

    투두리스트 함수형

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -4,7 +4,7 @@ import './TodoTemplate.scss';
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
-      <div className="app-title">일정관리</div>
+      <div className="app-title">오늘 할 일</div>
       <div className="content">{children}</div>
     </div>
   );
